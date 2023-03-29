41fe30adf41c2b5101dda69586a18160616296be
joyrudra-itobuz <joyrudra@itobuz.com>
2023-03-29 18:38:15 +0530

    Storing form Data

--- module/getPath.mjs
+++ module/getPath.mjs
@@ -6,8 +6,8 @@ export const findImage = (reqUrl) => {
   const pathArr = reqUrl.split("_");
   pathArr.unshift("database");
   pathArr[3] = pathArr[3] + ".jpg";
-  console.log(pathArr);
+  // console.log(pathArr);
   const imagePath = path.join(pathArr[0], pathArr[1], pathArr[2], pathArr[3]);
-  console.log(imagePath);
+  // console.log(imagePath);
   return imagePath;
 };
--- server.mjs
+++ server.mjs
@@ -2,12 +2,56 @@ import * as fs from "fs/promises";
 import * as path from "path";
 import * as http from "http";
 import * as getPath from "./module/getPath.mjs";
+import { parse } from "querystring";
+let chunks = "";
 
 const server = http.createServer(async (req, res) => {
-  if (req.url.length !== 0 && req.url.charAt(0) === "/") {
-    console.log("called!");
+  if (req.url === "/submit-date") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    req.on("data", (chunk) => {
+      chunks = chunk.toString();
+    });
+    req.on("end", async () => {
+      let parsedData = parse(chunks);
+      const filePath = path.join(
+        "database",
+        "booking-dates",
+        "booking-dates.txt"
+      );
+      await fs.appendFile(filePath, JSON.stringify(parsedData), {
+        encoding: "utf8",
+      });
+    });
+    res.write(chunks);
+    res.end();
+    chunks = "";
+    console.log("Submitting Now");
+  } else if (req.url === "/subcribe-data") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    req.on("data", (chunk) => {
+      chunks = chunk.toString();
+    });
+    req.on("end", async () => {
+      let parsedData = parse(chunks);
+      const filePath = path.join(
+        "database",
+        "subscriber-data",
+        "subcriber-emails.txt"
+      );
+      await fs.appendFile(filePath, JSON.stringify(parsedData), {
+        encoding: "utf8",
+      });
+    });
+    res.write(chunks);
+    res.end();
+    chunks = "";
+    console.log("Getting Email Now");
+  } else if (req.url.length !== 0 && req.url.charAt(0) === "/") {
     const imagePath = getPath.findImage(req.url);
-    console.log(imagePath);
     const imageBuffer = await fs.readFile(imagePath);
     res.setHeader("Content-Type", "image/jpeg");
     res.end(imageBuffer);
